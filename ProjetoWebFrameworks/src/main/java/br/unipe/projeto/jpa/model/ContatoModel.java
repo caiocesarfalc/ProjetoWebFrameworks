87f1643c1fcacdb2b0d07122563c0c44f03222a2
caiocesarfalc <caiocesarfalc@gmail.com>
2017-05-23 22:24:11 -0300

    Commit

--- ProjetoWebFrameworks/src/main/java/br/unipe/projeto/jpa/model/ContatoModel.java
+++ ProjetoWebFrameworks/src/main/java/br/unipe/projeto/jpa/model/ContatoModel.java
@@ -24,15 +24,6 @@ public class ContatoModel implements Serializable{
 	@Column(name="email")
 	private String email;
 	
-	private int idade;
-	
-	public int getIdade() {
-		return idade;
-	}
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-
 	public int getId() {
 		return id;
 	}
